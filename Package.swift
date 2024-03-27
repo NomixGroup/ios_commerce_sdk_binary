dc7dafae3dde641df80f896e1dd29d2e016cadd7
narcisbadea1 <narcisbadea@gmail.com>
2024-03-27 18:19:01 +0200

    0.2.3 binary

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppnomixCommerce",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppnomixCommerce",
+            targets: ["AppnomixCommerce"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppnomixCommerce",
+            url: "https://github.com/Shopnomix/ios_binary_sdk/releases/download/0.2.3.binary/AppnomixCommerce.xcframework.zip",
+            checksum: "ccc0777bf3e425c8719764fe2d730ac6bbf3ee94780d5f8ba21c1f8eedbcdd34"
+        ),
+    ]
+)
