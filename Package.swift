3952eb58772ae270755751ede438ee76bba3ae63
narcisbadea1 <narcisbadea@gmail.com>
2024-11-29 18:21:50 +0200

    1.2.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppnomixCommerce",
-            url: "https://github.com/NomixGroup/ios_commerce_sdk_binary/releases/download/1.2.0.7/AppnomixCommerce.xcframework.zip",
+            url: "https://github.com/NomixGroup/ios_commerce_sdk_binary/releases/download/1.2.0/AppnomixCommerce.xcframework.zip",
             checksum: "f5935589e43e9bdd2a21ecf3eb82a2a71428d57ca7ff22ce41be88404ddc04c8"
         ),
     ]
